c994ec6c888665285553eece4cf3f60376c7ab07
x-thanatos <xluobin@163.com>
2019-03-10 19:45:22 +0800

    refactor: use object distructing

--- components/topic-list/topic-item.js
+++ components/topic-list/topic-item.js
@@ -49,22 +49,19 @@ export default class TopicItem extends React.Component {
 
     render() {
         const { author, title, create_at, top, good, visit_count } = this.props.data
-        const { f5, h3, w3, mb2, mr2, mv1, mr1, mb1, pa2, tr, jcfe, flx_i, flx_row } = commonStyles
+        const { aic, f5, h3, w3, mb2, mr2, mt1, mb1, pa2, tr, jcsb, flx_i, flx_row } = commonStyles
         const item = (
-            <View style={[flx_row, mb2, pa2]}>
+            <View style={[flx_row, aic, mb2, pa2]}>
                 <Image style={[w3, h3, mr2]} source={{ uri: author.avatar_url }}/>
                 <View style={[flx_i]}>
                     <Text style={[f5, mb1]}>{title}</Text>
                     <Badge top={top} good={good}/>
-                    <View style={[flx_row, jcfe, mv1]}>
-                        <Text style={[styles.secondColor, mr1]}>
-                            {author.loginname}
-                        </Text>
+                    <View style={[flx_row, jcsb, mt1]}>
+                        <Text style={[styles.secondColor, tr]}>阅读: {visit_count}</Text>
                         <Text style={styles.secondColor}>
-                            {moment(create_at).format('YYYY-MM-DD HH:mm:ss')}
+                            {moment(create_at).format('YYYY/MM/DD HH:mm')}
                         </Text>
                     </View>
-                    <Text style={[styles.secondColor, tr]}>阅读: {visit_count}</Text>
                 </View>
             </View>
         )
